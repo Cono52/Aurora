2b85cde7f452ec7a1a217a098c1839b794406cb3
Conor O'Flanagan <Conor O'Flanagan>
2016-09-10 21:44:31 +0100

    created switcher component will be the base for the picture swiper

---
+++ src/app/switcher/switcher.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'switcher',
+  templateUrl: './switcher.component.html',
+  styles: [`
+    :host {
+        text-align:center;
+        display:block;
+    }
+  `]
+})
+export class SwitcherComponent {
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+   
+  }
+
+}
