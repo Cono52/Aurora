13eeefc4e73c6554e11712d3ed989815e1d92611
Cono52 <oflanac52@gmail.com>
2016-09-12 22:17:36 +0100

    styled buttons and also broke out css

--- src/app/switcher/switcher.component.ts
+++ src/app/switcher/switcher.component.ts
@@ -3,28 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'switcher',
   templateUrl: './switcher.component.html',
-  styles: [`
-    :host div{
-       display: flex;
-       align-items: center;
-       justify-content: center;
-    }
-    :host div button{
-       color: #e91e63;
-       display: flex;
-       align-items: center;
-       justify-content: center;
-       transparent; background-color: 
-       transparent; border-color: transparent;
-       transparent; cursor: default;
-    }
-
-    button:focus {outline:0;}
-
-    button:active i{
-      color: #fdb5ce;
-    }
-  `]
+  styleUrls: ['./switcher.component.css']
 })
 export class SwitcherComponent {
 
