f33a8cb07d8ec28ad5a12611df14c4a2974d20b5
Cono52 <oflanac52@gmail.com>
2016-10-09 14:50:23 +0100

    added home and profile icons

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,17 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nav',
   templateUrl: './nav.component.html',
-  styles: [`
-    :host {
-       background-color: #f48fb1;
-       font-family: 'Lobster', cursive;
-       margin-bottom:60px;
-    }
-    h1 {
-        text-align:center;
-        font-size:30px
-    }
-  `] 
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
 
